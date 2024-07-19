94540a7d694deff1153887fa000510dd07ca8afd
Osi Bluber <osibluber@protonmail.com>
2024-07-19 11:01:54 +0200

    refactor(home): replace custom theme source with bobthefish plugin

--- default/home.nix
+++ default/home.nix
@@ -127,12 +127,7 @@ in {
       # A nice theme
       {
         name = "eclm";
-        src = pkgs.fetchFromGitHub {
-          owner = "oh-my-fish";
-          repo = "theme-eclm";
-          rev = "185c84a41947142d75c68da9bc6c59bcd32757e7";
-          sha256 = "sha256-OBku4wwMROu3HQXkaM26qhL0SIEtz8ShypuLcpbxp78=";
-        };
+        src = pkgs.fishPlugins.bobthefish;
       }
     ];
     functions = {
