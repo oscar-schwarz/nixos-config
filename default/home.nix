8cb7335bd8077d08e86906c3726fb5d7f5915a35
Osi Bluber <osibluber@protonmail.com>
2024-07-19 10:55:41 +0200

    feat(home): add eclm theme plugin for fish shell

--- default/home.nix
+++ default/home.nix
@@ -123,6 +123,18 @@ in {
     interactiveShellInit = ''
       set fish_greeting
     '';
+    plugins = [
+      # A nice theme
+      {
+        name = "eclm";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "theme-eclm";
+          rev = "185c84a41947142d75c68da9bc6c59bcd32757e7";
+          sha256 = "sha256-OBku4wwMROu3HQXkaM26qhL0SIEtz8ShypuLcpbxp78=";
+        };
+      }
+    ];
     functions = {
       termgpt = ''
         ${lib.getExe heygptWrapper} --model "gpt-4o" """$argv""" | ${lib.getExe pkgs.glow}
