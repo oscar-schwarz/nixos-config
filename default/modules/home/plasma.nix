29f4c2977667ba496d3710719410452c4e2395a3
Osi Bluber <osibluber@protonmail.com>
2024-07-21 20:58:45 +0200

    refactor(plasma): update configuration syntax and enhance yakuake settings

--- default/modules/home/plasma.nix
+++ default/modules/home/plasma.nix
@@ -103,16 +103,16 @@ in
       # };
 
       # -- LOOK AND FEEL --
-      "ksplashrc" = {
-        "KSplash" = {
-          "Engine" = "None";
-          "Theme" = "None";
+      ksplashrc = {
+        KSplash = {
+          Engine = "None";
+          Theme = "None";
         };
       };
 
       # -- WIDGETS --
       # make task bar panel 60 wide
-      "plasmashellrc" = {
+      plasmashellrc = {
         "PlasmaViews/Panel 2/Defaults" = {
           thickness = "60";
           immutability = "1";
@@ -148,21 +148,52 @@ in
       };
       
       # -- yakuake --
-      "yakuakerc" = {
+      yakuakerc = {
         # skin
-        "Appearance" = {
+        Appearance = {
           Skin = "breeze-minimal";
           SkinInstalledWithKns = "true"; # Sometimes in life, lying is the way to go
         };
-        # do not occupy other windows
-        "Window"."KeepAbove" = "false";
-        "Window"."KeepOpen" = "true";
-        "Window"."ToggleToFocus" = "false";
-        "Window"."Height" = "100";
-        "Window"."Width" = "100";
-        "Window"."ShowTabBar" = "false";
-        "Desktop Entry"."DefaultProfile" = "${konsoleThemeName}.profile";
+
+        # Window appearance
+        Window = {
+          # don't occupy other windows
+          KeepAbove = "false";
+          # don't close when focus lost 
+          KeepOpen = "true";
+          ToggleToFocus = "true";
+
+          Height = "100";
+          Width = "100";
+
+          ShowTabBar = "false";
+        };
+
+        # Set theme
+        "Desktop Entry" = {
+          DefaultProfile = "${konsoleThemeName}.profile";
+        };
+
+        # Yakuake shortcuts
+        Shortcuts = {
+          # Creates a new session with 2x2 terminal grid
+          new-session-quad = "Ctrl+Shift+Up";
+
+          # Switches between sessions
+          next-session = "Ctrl+Shift+Right";
+          previous-session = "Ctrl+Shift+Left";
+
+          # Switches between terminal within a session
+          next-terminal = "Shift+Right";
+          previous-terminal = "Shift+Left";
+
+          # Set them to 'none' because their defaults conflict with above shortcuts
+          move-session-left = "none";
+          move-session-right = "none";
+        };
       };
+
+      # Autostart yakuake
       "autostart/org.kde.yakuake.desktop"."Desktop Entry" = desktopEntries.yakuake;
     };
   };
