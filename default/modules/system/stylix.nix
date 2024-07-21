e286af9b8b8068d371dfc1cabc078b150ef116b6
Osi Bluber <osibluber@protonmail.com>
2024-07-21 22:43:50 +0200

    fix(system): disable autoEnable in stylix configuration

--- default/modules/system/stylix.nix
+++ default/modules/system/stylix.nix
@@ -7,7 +7,7 @@ in {
     base16Scheme = "${pkgs.base16-schemes}/share/themes/${themeName}.yaml";
     image = ../../images/nms.jpg;
     polarity = "dark";
-    autoEnable = true;
+    autoEnable = false;
 
     opacity = {
       desktop = 0.5;
