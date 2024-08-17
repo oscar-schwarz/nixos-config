2b696f4e2418dc119d1696f3639745d2179b3dc1
Osi Bluber <osibluber@protonmail.com>
2024-08-17 10:21:58 +0200

    feat(configuration): add new SSH host and enable firewall on server

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -20,6 +20,7 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.firewall.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
