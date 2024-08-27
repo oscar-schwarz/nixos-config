3f55090a7c3450091d2081da7992220138e3568d
Osi Bluber <osibluber@protonmail.com>
2024-08-27 21:04:42 +0200

    feat(hyprland): add wofi launcher and enhance window manager settings

--- hosts/default/modules/home/hyprland.nix
+++ hosts/default/modules/home/hyprland.nix
@@ -1,6 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    wofi
+  ];
+
   programs.kitty = {
     enable = true;
   };
@@ -8,6 +12,27 @@
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
+      general = {
+        border_size = 3;
+      };
+
+      decoration = {
+        dim_inactive = true;
+        dim_strength = 0.3;
+      };
+
+      monitor = [
+        "eDP-1,preferred,auto,1"
+        "desc:DZX EVP-304 000000000000, preferred, auto, 1, transform, 3"
+      ];
+
+      bezier = [
+        "fast-in, 0.34, 0.12, 0.07, 0.96"
+      ];
+      animation = [
+        "windows, 1, 3, fast-in, popin"
+      ];
+
       input = {
         kb_layout = "us,de";
         kb_variant = "colemak,";
@@ -38,15 +63,17 @@
       in 
       (perDirectionLetter (dir: key: "$meta, ${key}, movefocus, ${dir}")) ++
       [
+        # application shortcuts
         "$meta, N, exec, kitty"
+        "$meta, E, exec, firefox"
+
+        # launcher
+        "$meta, O, exec, wofi --show drun"
+
+        # window management
         "$meta, W, killactive"
         "$meta, M, fullscreen"
-        "$meta, E, exec, firefox"
       ];
     };
-    extraConfig = ''
-      monitor = eDP-1,preferred,auto,1
-      monitor = desc:DZX EVP-304 000000000000, preferred, auto, 1, transform, 3
-    '';
   };
 }
