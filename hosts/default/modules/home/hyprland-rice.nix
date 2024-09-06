7e3d3bd07e0a5305c64b73ace7fc421009af1fa6
Osi Bluber <osibluber@protonmail.com>
2024-09-06 18:13:47 +0200

    fix(home): remove redundant 'with colors' statement in hyprland-rice.nix

--- hosts/default/modules/home/hyprland-rice.nix
+++ hosts/default/modules/home/hyprland-rice.nix
@@ -66,7 +66,7 @@ in {
         };
       };
     };
-    style = with colors.withHashtag; with colors;''
+    style = with colors.withHashtag;''
       /* Stylix colors */
       @define-color base00 ${base00}; @define-color base01 ${base01}; @define-color base02 ${base02}; @define-color base03 ${base03};
       @define-color base04 ${base04}; @define-color base05 ${base05}; @define-color base06 ${base06}; @define-color base07 ${base07};
