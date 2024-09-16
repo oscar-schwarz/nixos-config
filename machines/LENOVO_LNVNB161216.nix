af14722984ce72787c6946f522c8beea8bc945a6
Osi Bluber <osibluber@protonmail.com>
2024-09-16 19:59:50 +0200

    feat(machines): disable kernel image protection on LENOVO_LNVNB161216 for compatibility

--- machines/LENOVO_LNVNB161216.nix
+++ machines/LENOVO_LNVNB161216.nix
@@ -36,6 +36,7 @@
 
   # Power management
   powerManagement.enable = true;
+  security.protectKernelImage = false;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
