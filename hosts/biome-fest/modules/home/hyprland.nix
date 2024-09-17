75b818f29ff4b222600ac4102f259fc01a224a96
Osi Bluber <osibluber@protonmail.com>
2024-09-17 16:19:58 +0200

    refactor(home): remove redundant lock-session timeout configuration in hyprland module

--- hosts/biome-fest/modules/home/hyprland.nix
+++ hosts/biome-fest/modules/home/hyprland.nix
@@ -58,10 +58,6 @@
         MinsToSecs = mins: builtins.floor (mins*60);
       in [
         {
-          timeout = 10;
-          on-timeout = "loginctl lock-session";
-        }  
-        {
           timeout = MinsToSecs 1.5;
           on-timeout = "systemctl suspend";
         }
