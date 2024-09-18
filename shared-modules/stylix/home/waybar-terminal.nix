6277149ac5c50280ddded5cd52c5c65539f5e058
Osi Bluber <osibluber@protonmail.com>
2024-09-18 13:42:47 +0200

    fix(stylix): remove redundant width property from waybar-terminal configuration

--- shared-modules/stylix/home/waybar-terminal.nix
+++ shared-modules/stylix/home/waybar-terminal.nix
@@ -129,7 +129,6 @@ in {
       /* HIDE MODULES IN CERTAIN STATES */
       box#battery:not(.charging):not(.discharging) {
         transition: all 0.5s ease;
-        width: 0;
         opacity: 0;
       }
 
