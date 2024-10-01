1f8ac571aa4a6b5053f3ca7f51bb88941befacff
Osi Bluber <osibluber@protonmail.com>
2024-10-01 19:28:47 +0200

    fix(home): update matcha commands for better process management and waybar integration

--- shared-modules/home/matcha.nix
+++ shared-modules/home/matcha.nix
@@ -6,7 +6,7 @@
   ];
 
   # Run at the start of hyprland
-  wayland.windowManager.hyprland.settings.exec-once = [ "matcha --daemon --off" ];
+  wayland.windowManager.hyprland.settings.exec-once = [ "pkill matcha;matcha --daemon --off" ];
 
   # Waybar integration
   programs.waybar.settings.mainBar = {
@@ -15,7 +15,7 @@
     ];
 
     "custom/matcha" = {
-      exec = "matcha --toggle --bar=waybar";
+      exec = "matcha --toggle -b waybar";
     };
   };
 }
