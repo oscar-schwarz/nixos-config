6cc508bccca3478470c4d55c0dc968d5032adb49
Osi Bluber <osibluber@protonmail.com>
2024-10-02 12:32:56 +0200

    refactor(home): improve matcha integration with waybar and hyprland

--- shared-modules/home/matcha.nix
+++ shared-modules/home/matcha.nix
@@ -2,15 +2,8 @@
 
 {
   home.packages = [
-    (pkgs.writeShellScriptBin "matcha" ''
-        MATCHA_WAYBAR_OFF='&#xf186;&#xe163;\nDisabled'
-        MATCHA_WAYBAR_ON='&#xf7b6;&#xe163;\nEnabled'
-        ${pkgs.matcha}/bin/matcha "$@"
-    '')
+    pkgs.matcha
   ];
-
-  # Run at the start of hyprland
-  wayland.windowManager.hyprland.settings.exec-once = [ "pkill matcha;matcha --daemon" ];
 
   # Waybar integration
   programs.waybar.settings.mainBar = {
@@ -18,9 +11,29 @@
       "custom/matcha"
     ];
 
-    "custom/matcha" = {
-      exec = "matcha --toggle -b waybar";
-      interval = "once";
+    "custom/matcha" = let 
+      # Toggles matcha, kill if running, start if not running
+      toggleProgram = pkgs.writeShellScript "" ''
+        if [ '$(pidof matcha)' != "" ]; then
+          pkill matcha
+        else
+          matcha --deamon &
+        fi 
+      '';
+      # Checks whether match is running
+      statusCheck = pkgs.writeShellScript "" ''
+        if [ '$(pidof matcha)' != "" ]; then
+          echo -e '&#xf7b6;&#xe163;\nEnabled'
+        else
+          echo -e '&#xf186;&#xe163;\nDisabled'
+        fi
+      '';
+    in {
+      # Check status every second
+      exec = statusCheck;
+      interval = 1;
+      # Toggle on click
+      on-click = toggleProgram;
     };
   };
 }
