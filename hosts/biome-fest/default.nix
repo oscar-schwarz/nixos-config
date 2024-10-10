08e093e24e170b49e1de405c1566f4f6bacf320c
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-10-10 15:22:45 +0200

    fix(biome-fest): update UUID for fs1 in udisks2 settings

--- hosts/biome-fest/default.nix
+++ hosts/biome-fest/default.nix
@@ -16,7 +16,7 @@
   services.udisks2.settings = {
     "drives.conf" = {
       fs1 = {
-        match-device = "uuid:9debc741-b5d9-4721-a2bc-971008511283";
+        match-device = "uuid:1c9bb556-309f-4add-a7f0-723a3b96b2f6";
         mount-options = "default";
         mount-point = "/home/osi/files/remote";
       };
