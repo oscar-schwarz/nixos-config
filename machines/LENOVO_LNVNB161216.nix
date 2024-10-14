15810d44d9d16def62ad7419dacd3fa8d5384080
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-10-14 10:24:52 +0200

    feat(machines): enhance fingerprint service configuration with systemd settings

--- machines/LENOVO_LNVNB161216.nix
+++ machines/LENOVO_LNVNB161216.nix
@@ -36,6 +36,10 @@
   
   # Fingerprint
   services.fprintd.enable = true;
+  systemd.services.fprintd = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "simple";
+  };
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a;
   nixpkgs.config = {
