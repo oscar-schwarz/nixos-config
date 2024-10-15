f6603ee1f74e7513aa3584e8f9e3027b03a341e8
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-10-15 12:03:16 +0200

    feat(machines): enable systemd and FIDO2 for LUKS on LENOVO_LNVNB161216

--- machines/LENOVO_LNVNB161216.nix
+++ machines/LENOVO_LNVNB161216.nix
@@ -18,7 +18,14 @@
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-ff0fdffe-9e8d-4956-92ef-ce2317629a32".device = "/dev/disk/by-uuid/ff0fdffe-9e8d-4956-92ef-ce2317629a32";
+  boot.initrd = {
+    luks.devices."luks-ff0fdffe-9e8d-4956-92ef-ce2317629a32" = {
+      device = "/dev/disk/by-uuid/ff0fdffe-9e8d-4956-92ef-ce2317629a32";
+      # About key enrolling: https://nixos.org/manual/nixos/stable/#sec-luks-file-systems-fido2
+      crypttabExtraOpts = [ "fido2-device=auto" ];
+    };
+    systemd.enable = true;
+  };
 
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/8DFC-47EE";
