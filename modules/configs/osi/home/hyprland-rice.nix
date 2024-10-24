b887c05e48cb8febfd52990d941d6e4a72f792a1
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-10-24 15:38:28 +0200

    fix(configs): adjust shrink_percentage for focus animation in hyprland-rice.nix

--- modules/configs/osi/home/hyprland-rice.nix
+++ modules/configs/osi/home/hyprland-rice.nix
@@ -77,7 +77,7 @@ in {
         enabled = "yes";
         focus_animation = "shrink";
         shrink = {
-          shrink_percentage = 0.95;
+          shrink_percentage = 1.01;
         };
       };
     };
