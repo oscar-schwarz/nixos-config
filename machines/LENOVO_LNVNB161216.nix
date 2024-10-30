245e6c35011be6bf548c52eb441dcdcca84eeb63
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-10-30 18:06:25 +0100

    fix(machines): update fprintd driver package for Lenovo LNVNB161216 configuration

--- machines/LENOVO_LNVNB161216.nix
+++ machines/LENOVO_LNVNB161216.nix
@@ -65,10 +65,10 @@
   ];
   services.fprintd = {
     enable = true;
-    # package = pkgs.fprintd-tod;
+    package = pkgs.fprintd-tod;
     tod = {
       enable = true;
-      driver = pkgs.libfprint-2-tod1-goodix;
+      driver = pkgs.libfprint-2-tod1-goodix-550a;
     };
   };
 
