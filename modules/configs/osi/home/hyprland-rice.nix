0b846f1ade7c6d38416f62d9a0c5cb5944ec1784
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-11-11 17:33:49 +0100

    fix(configs): correct outer_color value in hyprland-rice configuration

--- modules/configs/osi/home/hyprland-rice.nix
+++ modules/configs/osi/home/hyprland-rice.nix
@@ -57,7 +57,7 @@ in {
       outline_thickness = border.width;
       rounding = border.radius;
 
-      outer_color = "rgb(${colors.base0D})";
+      outer_color = "rgb(${colors.base03})";
       inner_color = "rgb(${colors.base00})";
       font_color = "rgb(${colors.base05})"; 
       fail_color = "rgb(${colors.base08})";
