09dcfb8d4c64d3a10b6bf7b9b1826c186f2fa030
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-11-30 13:42:25 +0100

    chore(flakes): update flake.lock with latest revisions and hashes

--- modules/configs/osi/system/networking.nix
+++ modules/configs/osi/system/networking.nix
@@ -28,6 +28,7 @@
 
     openconnect.interfaces = {
       uni-leipzig-vpn = {
+        # Start with `sudo systemctl start openconnect-uni-leipzig-vpn`
         autoStart = false;
         gateway = "vpn.uni-leipzig.de";
         protocol = "anyconnect";
