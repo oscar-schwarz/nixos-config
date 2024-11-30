5597bc1f740c35e2eeb6bd801298edb3032a3ea9
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-11-30 15:16:41 +0100

    feat(hyprland): add workspace overview toggle and adjust panel height

--- modules/configs/osi/home/hyprland.nix
+++ modules/configs/osi/home/hyprland.nix
@@ -197,7 +197,7 @@
 
           # Rofi menus
           # launcher
-          "$meta, O, exec, pidof rofi || rofi -show drun"
+          "$meta, , exec, pidof rofi || rofi -show drun"
           # emoji
           "$meta, U, exec, pidof rofi || rofi -show emoji"
           # Pass
@@ -226,6 +226,9 @@
           # move window to workspaces
           "$meta_CTRL, J, movetoworkspace, r-1"
           "$meta_CTRL, H, movetoworkspace, r+1"
+
+          # workspace overview through hyprspace
+          "$meta, O, overview:toggle"
 
           # Taking screenshots
           "$meta_CTRL, A, exec, hyprshot -m window -m active --clipboard-only"
@@ -317,7 +320,7 @@
       # --- HYPRSPACE PLUGIN
       "plugin:overview" = {
         showEmptyWorkspaces = false;
-        panelHeight = 50;
+        panelHeight = 100;
       };
     };
 
