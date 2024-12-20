bad63f77686c64ccc0bbbacb5cf6411750cfe2fe
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-12-20 16:50:36 +0100

    fix(configs): correct regex escape sequence in waybar configuration

--- modules/configs/osi/home/waybar.nix
+++ modules/configs/osi/home/waybar.nix
@@ -132,7 +132,7 @@ in {
           rewrite = {
             # sometimes window classes formed like: com.github.xournalpp.xournalpp
             # this regex shows only the last string
-            "^(?:.+?\.)+(.+)$" = "$1";
+            "^(?:.+?\\.)+(.+)$" = "$1";
           };
           on-click = pkgs.writeShellScript "" ''
             CHOICE="$(echo -e 'Maximize\nFullscreen\nClose' | rofi -dmenu -p 'Window Action')"
