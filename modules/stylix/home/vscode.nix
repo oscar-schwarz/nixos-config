8bf230d6cd20727c075687b2eb87caf7c63d7837
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2025-01-14 18:10:17 +0100

    feat(stylix): update font sizes for applications in VSCode settings

--- modules/stylix/home/vscode.nix
+++ modules/stylix/home/vscode.nix
@@ -2,10 +2,10 @@
 with config.stylix.fonts;
 {
   programs.vscode.userSettings = {
-    "editor.fontSize" = lib.mkForce sizes.terminal;
-    "debug.console.fontSize" = lib.mkForce sizes.terminal;
-    "markdown.preview.fontSize" = lib.mkForce sizes.terminal;
-    "terminal.integrated.fontSize" = lib.mkForce sizes.terminal;
-    "chat.editor.fontSize" = lib.mkForce sizes.terminal;
+    "editor.fontSize" = lib.mkForce sizes.applications;
+    "debug.console.fontSize" = lib.mkForce sizes.applications;
+    "markdown.preview.fontSize" = lib.mkForce sizes.applications;
+    "terminal.integrated.fontSize" = lib.mkForce sizes.applications;
+    "chat.editor.fontSize" = lib.mkForce sizes.applications;
   };
 }
