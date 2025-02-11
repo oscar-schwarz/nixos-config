0b7cb1a139eae5c66d9a38687594a218d8a4a90b
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2025-02-11 12:16:50 +0100

    fix(waybar): initialize counter variable to prevent undefined behavior

--- modules/configs/osi/home/waybar.nix
+++ modules/configs/osi/home/waybar.nix
@@ -103,6 +103,7 @@ in {
         # Check if the cursor is on the edge of the screen if so, activate the waybar        
         if (( cursor_y == high_activate || cursor_y == low_activate)); then
           pidof waybar || waybar & # only launch if not launched already
+          counter=0
         fi
 
 
