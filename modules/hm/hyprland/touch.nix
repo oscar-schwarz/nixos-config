8d957d9af71eaaa261f940c39443894ce2256f41
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2025-04-14 18:54:15 +0200

    fix(hyprland): correct touch input configuration for tablet output

--- modules/hm/hyprland/touch.nix
+++ modules/hm/hyprland/touch.nix
@@ -10,7 +10,7 @@
     settings = {
       # Touch input is sent to the touchscreen
       input.touchdevice.output = "eDP-1";
-      input.touch.output = "eDP-1";
+      input.tablet.output = "eDP-1";
 
       # settings for above plugin
       "plugin:touch-gestures" = {
