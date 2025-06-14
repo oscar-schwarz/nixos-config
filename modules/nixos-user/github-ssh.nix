fb39e0155fdadfa1e7201987f6aad1b40a1eba5b
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2025-06-14 14:02:41 +0200

    Successful Rebuild

--- modules/nixos-user/github-ssh.nix
+++ modules/nixos-user/github-ssh.nix
@@ -5,6 +5,7 @@ username: {
   sops.secrets = {
     "ssh-keys/rsa_github_osipog/private" = {owner = username;};
     "ssh-keys/rsa_github_os/private" = {owner = username;};
+    "ssh-keys/osi/private" = {owner = username;};
   };
 
   programs.ssh.extraConfig = ''
@@ -19,6 +20,12 @@ username: {
       User git
       IdentityFile ${config.getSopsFile "ssh-keys/rsa_github_os/private"}
       IdentitiesOnly yes
+
+    Host ag-link-hyperjump
+      HostName hyperjump.reudnetz.org
+      Port 21016
+      User ${username}
+      IdentityFile ${config.getSopsFile "ssh-keys/osi/private"}
 
     # Allow overwriting the values above
     Include ${config.home-manager.users.${username}.home.homeDirectory}/.ssh/config
