338602e93492d6976529c61aabfb91a297da7e58
Oscar Schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2025-06-19 20:46:57 +0200

    Successful Rebuild

--- modules/nixos/themes/prismarine/hm/fastfetch.nix
+++ modules/nixos/themes/prismarine/hm/fastfetch.nix
@@ -6,8 +6,8 @@
 }: let
   tintedNixosSvg = pkgs.callPackage (import ../../../../../lib/tint-nixos-svg.nix config.lib.stylix.colors) {};
 in {
-  programs.fastfetch.settings.logo = {
-    width = lib.mkDefault 40;
-    source = tintedNixosSvg;
-  };
+  # programs.fastfetch.settings.logo = {
+  #   width = lib.mkDefault 40;
+  #   source = tintedNixosSvg;
+  # };
 }
